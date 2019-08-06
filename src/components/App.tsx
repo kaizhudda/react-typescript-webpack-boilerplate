6858fe232749e9d2209153dbeb81fe8a28ab48fa
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-08-06 22:46:49 +1200

    Added Eslint with prettier and rules, need to tweak them

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
-import { IAppProps } from '../interfaces/AppInterface';
+import PropTypes from 'prop-types';
+import { AppProps } from '../interfaces/AppInterface';
 
-export const App: React.FC<IAppProps> = (props): JSX.Element => {
+export const App: React.FC<AppProps> = (props): JSX.Element => {
+  const { complier, framework } = props;
   return (
     <h1>
-      Hello from {props.complier} and {props.framework}
+      Hello from {complier} and {framework}
     </h1>
   );
+};
+
+App.propTypes = {
+  complier: PropTypes.string,
+  framework: PropTypes.string
 };
--- src/interfaces/AppInterface.ts
+++ src/interfaces/AppInterface.ts
@@ -1,4 +1,4 @@
-export interface IAppProps {
+export interface AppProps {
   complier: string;
   framework: string;
 }
