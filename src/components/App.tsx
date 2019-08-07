3d0f595cbfd1651187a186d837ccc74c502fc7a4
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-08-07 21:10:40 +1200

    optimising webpack bundle

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import { AppProps } from '../interfaces/AppInterface';
 
+import './App.scss';
+
 export const App: React.FC<AppProps> = (props): JSX.Element => {
   const { complier, framework } = props;
   return (
-    <h1>
+    <h1 className="App">
       Hello from {complier} and {framework}
     </h1>
   );
