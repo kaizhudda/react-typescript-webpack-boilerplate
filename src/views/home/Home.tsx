75f6442ffd5736ad06052b90958e27f6286f35b5
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-08-11 20:41:40 +1200

    React Hetlmet added for SERo

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,6 +1,19 @@
 import * as React from 'react';
+import { Helmet } from 'react-helmet';
 import { RouteComponentProps } from 'react-router-dom';
 
 export const Home: React.FC<RouteComponentProps> = (): JSX.Element => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <Helmet>
+        <title>About</title>
+        <meta
+          charSet="utf-8"
+          name="description"
+          content="We are building a super lightweight react boilerplate using webpack"
+        />
+      </Helmet>
+      Homepage
+    </div>
+  )
 };
